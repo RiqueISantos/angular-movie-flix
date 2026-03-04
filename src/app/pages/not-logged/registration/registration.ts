291d4e58c05dc194d23070ff0e9fb4a3dbeaa4fa
Henrique Oliveira <henrique.oliveira@entrepay.com.br>
2026-03-04 15:15:14 -0300

    feat: adiciona tela de cadastro

--- src/app/pages/not-logged/registration/registration.ts
+++ src/app/pages/not-logged/registration/registration.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../components/not-logged/header/header';
-import { SignIn } from '../../../components/not-logged/sign-in/sign-in';
+import { Signup } from '../../../components/not-logged/sign-up/sign-up';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ Header, SignIn],
+  imports: [ Header, Signup],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
 })
